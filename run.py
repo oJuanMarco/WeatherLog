61433c40f5b4b32e5966be49057401af5da04f8f
Juan <juanbusine22@gmail.com>
2026-08-30 19:59:18 -0300

    API studies done

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import openmeteo_requests
-
 import pandas as pd
 import requests_cache
 from retry_requests import retry
@@ -7,7 +6,7 @@ from retry_requests import retry
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+openmeteo = openmeteo_requests.Client(session = cache_session)
 
 # Make sure all required weather variables are listed here
 # The order of variables in hourly or daily is important to assign them correctly below
@@ -16,23 +15,20 @@ params = {
 	"latitude": -10,
 	"longitude": -55,
 	"daily": ["temperature_2m_max", "temperature_2m_min"],
-	"hourly": ["temperature_2m", "rain", "apparent_temperature"],
+	"hourly": ["temperature_2m", "apparent_temperature","rain"],
 	"timezone": "America/Sao_Paulo"
 }
 responses = openmeteo.weather_api(url, params = params)
 
 # Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
-print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation: {response.Elevation()} m asl")
-print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+print(f"Horário de: {response.Timezone()}")
 
 # Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
 hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-hourly_rain = hourly.Variables(1).ValuesAsNumpy()
 hourly_apparent_temperature = hourly.Variables(2).ValuesAsNumpy()
+hourly_rain = hourly.Variables(1).ValuesAsNumpy()
 
 hourly_data = {
 	"date": pd.date_range(
@@ -43,29 +39,9 @@ hourly_data = {
 	).tz_convert(response.Timezone().decode())
 }
 
-hourly_data["temperature_2m"] = hourly_temperature_2m
-hourly_data["rain"] = hourly_rain
+hourly_data["temperature"] = hourly_temperature_2m
 hourly_data["apparent_temperature"] = hourly_apparent_temperature
+hourly_data["rain"] = hourly_rain
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
 print("\nHourly data\n", hourly_dataframe)
-
-# Process daily data. The order of variables needs to be the same as requested.
-daily = response.Daily()
-daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-
-daily_data = {
-	"date": pd.date_range(
-		start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-		end =  pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-		freq = pd.Timedelta(seconds = daily.Interval()),
-		inclusive = "left"
-	).tz_convert(response.Timezone().decode())
-}
-
-daily_data["temperature_2m_max"] = daily_temperature_2m_max
-daily_data["temperature_2m_min"] = daily_temperature_2m_min
-
-daily_dataframe = pd.DataFrame(data = daily_data)
-print("\nDaily data\n", daily_dataframe)
